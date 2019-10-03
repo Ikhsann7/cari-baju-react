0177d6c15648329a8f17e7ec51544c7b3e502d42
Ahmad <yusufadinata19@gmail.com>
2019-10-03 14:46:22 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./container/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
